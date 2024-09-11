fde0835638af7ff8440113ec2e82ed447600c5f9
well016 <well016@mail.ru>
2024-09-11 16:31:52 +0300

    Инициализировал параметры для окна Добавил функцию триггер кнопок Добавил функцию парсинга las файла

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-from PySide6.QtWidgets import QApplication,QMainWindow
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
 
-from ui_main import Ui_mainWindow
+from Design.ui_main import Ui_mainWindow
+import lasio
 
 
-class LasMaster(QMainWindow,Ui_mainWindow):
+class LasMaster(QMainWindow):
     def __init__(self):
         super(LasMaster, self).__init__()
-        self.setupUi(self)
+
+        self.ui = Ui_mainWindow()
+        self.ui.setupUi(self)
+        self.add_function()
+
+    def add_function(self):
+        self.ui.import_p.triggered.connect(self.import_las)
+        self.ui.version_p.triggered.connect(self.import_las)
+
+    def import_las(self):
+        try:
+            file_name = QFileDialog.getOpenFileName(self, 'Open file', '.', 'las files (*.las)')[0]
+            las = lasio.read(file_name)
+            print(las)
+        except:
+            return
 
 
 
 if __name__ == "__main__":
-    app = QApplication([])
-    window = LasMaster()
-    window.show()
-    app.exec()
+    app = QApplication(sys.argv)
+    LasMaster = LasMaster()
+    LasMaster.show()
+    sys.exit(app.exec())
